f1ae2fd58732754fa3ec537f9ee25a80c350ec15
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-01-08 16:38:58 +0530

    Create partha.c

---
+++ partha.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    // your code goes here
+    int num;
+    scanf("%d",&num);
+    printf("INPUT %d\n",num);
+    if(num>0)
+    printf("OUTPUT \n postive");
+    else if(num<0)
+    printf("OUTPUT \n negative");
+    else
+    printf("OUTPUT\n zero");
+    return 0;
+}
+
