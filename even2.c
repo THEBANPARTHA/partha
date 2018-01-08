2974783613f5e5d55ee7af665c7c4ad2761af959
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-01-08 19:35:00 +0530

    Create even2.c

---
+++ even2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a,b;
+    
+    scanf("%d",&b);
+    printf("INPUT\n %d",b);
+    a=b%2;
+    if(a==0)
+    
+    printf("\nOUTPUT \neven",a);
+    
+    else
+    printf("\nOUTPUT\nodd",a);
+        
+    
+    return 0;
+}
