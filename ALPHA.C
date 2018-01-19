fc22559e158369fd9242119bba693e5a2ed8f31b
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-01-19 16:52:36 +0530

    Create ALPHA.C

---
+++ ALPHA.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<string.h>
+
+int main(void) {
+int n,i=0;
+char a[50];
+scanf("%s",&a[i]);
+printf("INPUT \n %s \n",a);
+n=strlen(a);
+printf("OUTPUT\n");
+for(i=0;i<=n;i++)
+{
+if(a[i]=='a' || a[i]=='b' || a[i]=='c' || a[i]=='d' || a[i]=='e'||a[i]=='f' || a[i]=='g' || a[i]=='h' || a[i]=='i' || a[i]=='j'||a[i]=='k' || a[i]=='l' || a[i]=='m' || a[i]=='n' || a[i]=='o'||a[i]=='p' || a[i]=='q' || a[i]=='r' || a[i]=='s' || a[i]=='t'||a[i]=='u' || a[i]=='v' || a[i]=='w' || a[i]=='x' || a[i]=='y'||a[i]=='z'||a[i]=='A' || a[i]=='B' || a[i]=='C' || a[i]=='D' || a[i]=='E'||a[i]=='F' || a[i]=='G' || a[i]=='H' || a[i]=='I' || a[i]=='J'||a[i]=='K' || a[i]=='L' || a[i]=='M' || a[i]=='N' || a[i]=='O'||a[i]=='P' || a[i]=='Q' || a[i]=='R' || a[i]=='S' || a[i]=='T'||a[i]=='U' || a[i]=='V' || a[i]=='W' || a[i]=='X' || a[i]=='Y'||a[i]=='Z')
+printf("yes");
+else
+printf("no");
+return 0;
+}
+}
