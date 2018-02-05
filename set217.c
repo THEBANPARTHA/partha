a0c6a0ea210cdf3871bd0d4ce28bbeab1b62ed5e
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-02-05 16:19:32 +0530

    Create set217.c

---
+++ set217.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+int number, originalNumber, remainder, result = 0;
+scanf("%d",&number);
+ printf("INPUT\n%d",number);
+
+originalNumber = number;
+while (originalNumber != 0)
+{
+remainder = originalNumber%10;
+result += remainder*remainder*remainder;
+originalNumber /= 10;
+}
+if(result == number)
+printf("\nOUTPUT\nyes");
+else
+printf("\nOUTPUT\nno");
+return 0;
+}
