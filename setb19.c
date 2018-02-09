5e2a7ed3e6a020645580b897088c53a7f2df9a8b
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-02-09 10:34:09 +0000

    Create setb19.c

---
+++ setb19.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+int a,i,b=1;
+scanf("%d",&a);
+printf("INPUT\n%d",a);
+printf("\nOUTPUT\n");
+for(i=1;i<=a;i++)
+{
+ b*=i;
+ 
+}printf("%d",b);
+
+	
+
+return 0;
+}
