f4f428b7b8ef52d78ba0147e8dea5d07f96f9ea9
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-02-12 11:25:09 +0000

    Create setc32.c

---
+++ setc32.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int count=0,i;
+char str[100];
+printf("INPUT\n");
+scanf("%s",&str[i]);
+for(i=0;str[i]=0;i++);
+{
+if(str[i]=' ')
+{
+++count;
+}
+printf("\nOUTPUT\n%d",count+1);
+}
+}
