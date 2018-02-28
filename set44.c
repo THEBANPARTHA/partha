a0ecabd91a9b416622fb2ad1d6596930e93ec9de
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-02-28 11:26:32 +0000

    Create set44.c

---
+++ set44.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	int a,b;
+	scanf("%d",&a);
+	printf("INPUT\n%d",a);
+	if(a>=1&&a<=10)
+	printf("\nOUTPUT\nyes");
+	else
+	printf("\nOUTPUT\nno");
+	return 0;
+}
