220842384bbb2912d9d52ee568f2d379196089c9
THEBANPARTHA <35135860+THEBANPARTHA@users.noreply.github.com>
2018-03-14 11:12:46 +0000

    Create set52.c

---
+++ set52.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main(void) {
+	int n ;
+	scanf("%d",&n);
+	printf("INPUT\n%d",n);
+	switch (n)
+	{
+	  case 1:
+	  printf("\nOUTPUT\none");
+	  break;
+	   case 2:
+	  printf("\nOUTPUT\ntwo");
+	  break;
+	  case 3:
+	  printf("\nOUTPUT\nthree");
+	  break;
+	   case 4:
+	  printf("\nOUTPUT\nfour");
+	  break;
+	   case 5:
+	  printf("\nOUTPUT\nfive");
+	  break;
+	   case 6:
+	  printf("\nOUTPUT\nsix");
+	  break;
+	   case 8:
+	  printf("\nOUTPUT\neight");
+	  break;
+	   case 9:
+	  printf("\nOUTPUT\nnine");
+	  break;
+	   case 10:
+	  printf("\nOUTPUT\nten");
+	  break;
+	}
+	
+	return 0;
+}
